8cfe33587b85c66a0a8bacd962c0d79d08bf6787
VICKY200018 <vickygangadharan@gmail.com>
2024-02-06 23:07:57 +0530

    added to the file

---
+++ Q8.js
@@ -0,0 +1,43 @@
+// Write a code to get the input in the given format and print the output in the given format.
+
+// Input Description:
+// A single line contains three float values separated by space.
+
+// Output Description:
+// Print the float value separated by line.
+
+// Sample Input :
+// 2.3 4.5 7.8
+// Sample Output :
+// 2.3
+// 4.5
+// 7.8
+
+//Solution
+
+/// Getting input via STDIN
+const readline = require("readline");
+
+const inp = readline.createInterface({
+  input: process.stdin
+});
+
+const userInput = [];
+
+inp.on("line", (data) => {
+  userInput.push(data);
+});
+
+inp.on("close", () => {
+  //start-here
+  //Your code goes here … replace the below line with your code logic 
+
+  const arr = userInput[0].split(" ");
+  let i = 0;
+  while(i < arr.length) {
+      console.log(arr[i]);
+      i++;
+  }
+  
+  //end-here
+});
